c9d53feaac3f70f2e94b3fd498826c0b52e48bef
4z4n <154474515+4z4n@users.noreply.github.com>
2024-12-27 23:30:00 +0700

    Add files via upload

---
+++ Undangan/script.js
@@ -0,0 +1,5 @@
+// Menambahkan efek interaktif saat halaman dimuat
+window.addEventListener('load', function() {
+    const rsvpButton = document.querySelector('.rsvp-button');
+    rsvpButton.classList.add('rsvp-appear');
+});
